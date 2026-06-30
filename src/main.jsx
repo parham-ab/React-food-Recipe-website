049ffc06b59f01aff9df6bcce7263c3609f22f7d
Parham Abolghasemi <parhamab17@gmail.com>
2026-06-30 21:59:00 +0330

    migrated from CRA to vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-// Styles
 import "./assets/styles/index.scss";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
 );
